9aae60ccac4210a3dc9e6d55b6324a76f895d522
cenzwong <44856918+cenzwong@users.noreply.github.com>
2019-06-20 16:07:10 +0800

    Update EventTimer.ino

--- EventTimer.ino
+++ EventTimer.ino
@@ -6,6 +6,7 @@ typedef struct EventRegister{
   unsigned long duration;
 };
 
+//initial the start millis, and the time to repeat(in ms)
 EventRegister ledToggle{0, false, 1000};
 
 void setup() {
@@ -22,6 +23,12 @@ void loop() {
     //toggle LED function flag on
     ledToggle.check_flag = true;
   }
+  /*
+  if(current_millis - __EventRegister__.previous_millis >= __EventRegister__.duration){
+    //toggle LED function flag on
+    __EventRegister__.check_flag = true;
+  }
+  */
 
   //===========do()===========
   if(ledToggle.check_flag){
@@ -29,7 +36,16 @@ void loop() {
     //reset the flag and timer
     ledToggle.check_flag = false;
     ledToggle.previous_millis = current_millis;
+    //ledToggle.duration = 200;  //<--- make it faster
   }
+  /*
+  if(__EventRegister__.check_flag){
+    led_toggle(led_pin);
+    //reset the flag and timer
+    __EventRegister__.check_flag = false;
+    __EventRegister__.previous_millis = current_millis;
+  }
+  */
 }
 
 void led_toggle(unsigned char pin){
